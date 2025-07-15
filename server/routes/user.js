dd06caedb5620d1dd53d2f925c445260117fdc4d
kIngsLaYeR-gt <akshanshkumar155@gmail.com>
2025-07-15 14:01:00 +0530

    Final Commit

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,8 +2,9 @@ import express from "express";
 import { login, logout, register } from "../controllers/user.js";
 import isAuthenticated from "../middleware/isAuthentiated.js";
 
-const router = express.Router();
 
+// Routes
+const router = express.Router();
 router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/logout").get(logout)
